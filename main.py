0821311d405396a2469f257e732b74c28094a699
BuildTools <unconfigured@null.spigotmc.org>
2019-10-08 16:52:21 -0500

    added price checker and automated emails. emails are sent at a set price threshold.

--- main.py
+++ main.py
@@ -1,13 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import re
 
-URL = "https://www.amazon.com/gp/product/B07J4SXNQ5/ref=ox_sc_saved_title_4?smid=ATVPDKIKX0DER&psc=1"
+URL = "https://www.amazon.com/gp/product/B07598SX6S/ref=ox_sc_saved_title_7?smid=ATVPDKIKX0DER&psc=1"
 
 headers = {
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+    "like Gecko) Chrome/77.0.3865.90 Safari/537.36"}
 
-page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def check_price():
 
-print(soup.prettify())
+    page = requests.get(URL, headers=headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title = soup2.find(id="productTitle").get_text()
+
+    price = soup2.find(id="priceblock_ourprice").get_text()
+    fixed_price = re.sub('[^a-zA-Z0-9 \n\.]', '', price)
+    converted_price = float(fixed_price[0:5])
+
+    if converted_price < 0.200:
+        send_mail()
+
+    print(converted_price)
+    print(title.strip())
+
+    if converted_price < 0.200:
+        send_mail()
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('username@gmail.com', '**********')
+
+    subject = 'Price fell down!'
+    body = 'Check the amazon link: ' \
+           'https://www.amazon.com/gp/product/B07598SX6S/ref=ox_sc_saved_title_7?smid=ATVPDKIKX0DER&psc=1'
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        'sendingusername@gmail.com',
+        'receivingusername@gmail.com',
+        msg
+    )
+    print('EMAIL HAS BEEN SENT')
+
+    server.quit()
+
+
+check_price()
